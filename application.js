320e080a6d7531a86ceb7ea11f9cb3aacbffb713
Alex Benoit <alex@lewagon.com>
2017-12-26 11:53:05 +1100

    done create and update

--- application.js
+++ application.js
@@ -1,4 +1,6 @@
 // TODO validation on forms?
+// TODO responsive
+
 // const apiUrl = 'https://ideaboard-rails-api.herokuapp.com';
 const apiUrl = 'http://localhost:3000';
 const containerId = 'ideas-container';
@@ -20,14 +22,22 @@ const buildBlankIdea = () => {
         <span>Add New Idea</span>
       </div>
       <div class="hidden" id="new-idea-form">
-        <input type="text" class="idea-title" placeholder="Product Hunt">
-        <textarea class="idea-body" rows="5" placeholder="Daily curation of the best new products"></textarea>
+        <input type="text" class="new-idea-title" placeholder="Product Hunt">
+        <textarea class="new-idea-body" rows="5" placeholder="Daily curation of the best new products"></textarea>
         <button class="idea-save-button">Save</button>
         <button class="idea-cancel-button">Cancel</button>
       </div>
     </div>
   `;
 };
+
+const handleErrors = (response) => {
+  if (!response.ok) {
+    throw Error(response.statusText);
+  }
+  return response;
+};
+
 
 const toggleNewIdeaForm = () => {
   const newIdeaForm = document.getElementById('new-idea-form');
@@ -36,39 +46,74 @@ const toggleNewIdeaForm = () => {
   newIdeaForm.classList.toggle('hidden');
   newIdeaButton.classList.toggle('hidden');
 
-  const ideaTitleInput = document.querySelector('.idea-title');
+  const ideaTitleInput = document.querySelector('.new-idea-title');
   ideaTitleInput.focus();
+};
+
+const createIdea = () => {
+  const newIdeaTitle = document.querySelector('.new-idea-title');
+  const newIdeaBody = document.querySelector('.new-idea-body');
+  const newIdeaContainer = document.querySelector('.new-idea-container');
+  const requestBody = JSON.stringify({ title: newIdeaTitle.value, body: newIdeaBody.value });
+
+  fetch(`${apiUrl}/ideas`, {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'POST',
+    body: requestBody
+  })
+    .then(handleErrors)
+    .then(response => response.json())
+    .then((data) => {
+      newIdeaContainer.insertAdjacentHTML('afterend', buildIdea(data));
+      newIdeaTitle.value = '';
+      newIdeaBody.value = '';
+      toggleNewIdeaForm();
+    })
+    // ADD NOTIFICATION HERE
+    .catch(error => console.log('There was an error!', error));
+};
+
+const getUpdateRequestBody = (event) => {
+  if (event.target.classList.contains('idea-title')) {
+    return JSON.stringify({ title: event.target.value });
+  } else if (event.target.classList.contains('idea-body')) {
+    return JSON.stringify({ body: event.target.value });
+  }
+};
+
+const updateIdea = (event) => {
+  const ideaElement = event.target.parentElement;
+  const ideaId = ideaElement.dataset.ideaId;
+  const requestBody = getUpdateRequestBody(event);
+
+  fetch(`${apiUrl}/ideas/${ideaId}`, {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'PATCH',
+    body: requestBody
+  })
+    .then(handleErrors)
+    // .then()
+    // ADD NOTIFICATION HERE
+    .catch(error => console.log('There was an error!', error));
 };
 
 const deleteIdea = (event) => {
   if (!event.target.classList.contains('idea-delete-button')) {
     return;
   }
+
   const ideaElement = event.target.parentElement;
   const ideaId = ideaElement.dataset.ideaId;
   const confirmation = window.confirm('Are you sure you want to delete this idea?');
+
   if (confirmation) {
-    fetch(`${apiUrl}/ideas/${ideaId}`, { method: 'delete' })
-      .then(ideaElement.remove());
+    fetch(`${apiUrl}/ideas/${ideaId}`, {
+      method: 'DELETE'
+    })
+      .then(handleErrors)
+      .then(() => ideaElement.remove())
+      .catch(error => console.log('There was an error!', error));
   }
-};
-
-const createIdea = () => {
-  const newIdeaTitle = document.querySelector('.idea-title').value;
-  const newIdeaBody = document.querySelector('.idea-body').value
-  const requestBody = JSON.stringify({ title: newIdeaTitle, body: newIdeaBody })
-
-  fetch(`${apiUrl}/ideas`, {
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    method: 'post',
-    body: requestBody
-  })
-    .then(response => response.json())
-    .then((data) => {
-      console.log(data);
-    });
 };
 
 const addEventListeners = () => {
@@ -80,6 +125,7 @@ const addEventListeners = () => {
 
   const ideasContainer = document.getElementById('ideas-container');
   ideasContainer.addEventListener('click', deleteIdea);
+  ideasContainer.addEventListener('focusout', updateIdea);
 
   const saveNewIdeaButton = document.querySelector('.idea-save-button');
   saveNewIdeaButton.addEventListener('click', createIdea);
@@ -90,6 +136,7 @@ const fetchIdeas = () => {
   let ideas = '';
   ideas += buildBlankIdea();
   fetch(`${apiUrl}/ideas`)
+    .then(handleErrors)
     .then(response => response.json())
     .then((data) => {
       data.forEach((idea) => {
@@ -97,7 +144,8 @@ const fetchIdeas = () => {
       });
       container.innerHTML = ideas;
       addEventListeners();
-    });
+    })
+    .catch(error => console.log('There was an error!', error));
 };
 
 document.addEventListener('DOMContentLoaded', fetchIdeas);
